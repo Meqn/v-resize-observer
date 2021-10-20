27dbe2b51719011e48e320ff61018868e26c9966
Mervin <mengqing723@gmail.com>
2021-10-20 22:00:49 +0800

    :bug: 修复因babel配置导致引入文件报错

--- build/rollup.config.base.js
+++ build/rollup.config.base.js
@@ -1,7 +1,7 @@
 import { babel } from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import vue from 'rollup-plugin-vue'
-import cjs from '@rollup/plugin-commonjs'
+import commonjs from '@rollup/plugin-commonjs'
 import replace from '@rollup/plugin-replace'
 import postcss from 'rollup-plugin-postcss'
 import analyze from 'rollup-plugin-analyzer'
@@ -17,12 +17,12 @@ export default {
     vue({
       css: true,
     }),
+    commonjs(),
     babel({
       exclude: 'node_modules/**',
       babelHelpers: 'runtime',
       extensions: ['.js', '.vue'],
     }),
-    cjs(),
     replace({
       VERSION: JSON.stringify(config.version),
       preventAssignment: false,
